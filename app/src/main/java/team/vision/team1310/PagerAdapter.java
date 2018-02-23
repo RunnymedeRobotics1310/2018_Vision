177aa2ca371cfadf8b1702756b9b7e26615b8e89
Rishab Bhatt <rishabswift@gmail.com>
2018-02-23 18:27:33 -0500

    Initial Commit

---
+++ app/src/main/java/team/vision/team1310/PagerAdapter.java
@@ -0,0 +1,38 @@
+package team.vision.team1310;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.numOfTabs = numberOfTabs;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
